d422c37210e7728fc3bba3443da2805f53b8c1d7
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-08-23 23:21:31 +0200

    Implement dynamic grid creation and mouseover effects based on input value

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const gridContainer = document.querySelector(".gridContainer");
+const inputEl = document.querySelector("input");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement("div")
-    square.classList.add("square")
-    gridContainer.appendChild(square)
-    
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black"
-    })
+inputEl.addEventListener("change", () => {
+ createGrid((inputEl.value));
+})
+
+
+
+const createGrid = (inputValue) => {
+     gridContainer.innerHTML = "";
+
+     for (let i = 0; i < inputValue * inputValue; i++) {
+       const square = document.createElement("div");
+       square.classList.add("square");
+       gridContainer.appendChild(square);
+
+       gridContainer.style.gridTemplateColumns = `repeat(${inputValue}, 1fr)`;
+       gridContainer.style.gridTemplateRows = `repeat(${inputValue}, 1fr)`;
+
+       square.addEventListener("mouseover", () => {
+
+        if (square.style.backgroundColor === "black") {
+            square.style.opacity = "0.9";
+        } else {
+            square.style.backgroundColor = "black";
+        }
+       });
+     }
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  createGrid(inputEl.value = 16);
+});
